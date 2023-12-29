304d0ec1f32b592b4fb5df40cb3eb9071280cd1e
Adel <kovacsadel12@gmail.com>
2023-12-29 16:42:26 +0100

    feat: allow * access to filament

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Gate;
-
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -23,9 +21,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('viewFilament', function ($user) {
-            // Return true to allow all authenticated users to access Filament
-            return true;
-        });
+        //
     }
 }
